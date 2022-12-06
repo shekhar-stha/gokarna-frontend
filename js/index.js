52a19fe2e7868d9d485402e312c03f71336a629a
Shekhar <shekhar.shrestha@asparksys.com>
2022-12-06 16:38:41 +0545

    feat: added contact page

---
+++ js/index.js
@@ -0,0 +1,37 @@
+$('.team-members').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    loop: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<i class="fa-solid fa-chevron-left left-arrow"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right right-arrow">',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
