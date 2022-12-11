633f5f82d86d976d843c6b6682800a914cd9e9af
Shekhar <shekhar.shrestha@asparksys.com>
2022-12-11 16:38:47 +0545

    rooms

--- js/index.js
+++ js/index.js
@@ -82,33 +82,32 @@ $('.room-images').slick({
     loop: true,
     autoplay: true,
     autoplaySpeed: 2000,
-    dots: false
-    // prevArrow: '<i class="fa-solid fa-chevron-left left-arrow"></i>',
-    // nextArrow: '<i class="fa-solid fa-chevron-right right-arrow">',
-    // responsive: [
-    //     {
-    //         breakpoint: 992,
-    //         settings: {
-    //             slidesToShow: 3,
-    //             slidesToScroll: 2,
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 768,
-    //         settings: {
-    //             slidesToShow: 2,
-    //             slidesToScroll: 2,
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 480,
-    //         settings: {
-    //             slidesToShow: 1,
-    //             slidesToScroll: 1
-    //         }
-    //     }
-    //     // You can unslick at a given breakpoint now by adding:
-    //     // settings: "unslick"
-    //     // instead of a settings object
-    // ]
+    prevArrow: '<i class="fa-solid fa-chevron-left left-arrow"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right right-arrow">',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
 });
